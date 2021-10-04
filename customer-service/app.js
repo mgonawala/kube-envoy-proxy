8c52725ad74bcd7e53ae034c7fdee930806c0919
mgonawala <mohini.gonawala90@gmail.com>
2021-10-04 21:01:55 +0100

    delete unwanted

--- customer-service/app.js
+++ customer-service/app.js
@@ -1,29 +1,24 @@
-import fetch from 'node-fetch'
-import express from 'express'
-
+import fetch from "node-fetch";
+import express from "express";
 
 // Constants
 const PORT = 8080;
-const HOST = '0.0.0.0';
+const HOST = "0.0.0.0";
 
 // App
 const app = express();
-app.get('/customers/:id', async (req, res) => {
-  let {id} = req.params;
+app.get("/customers/:id", async (req, res) => {
+  let { id } = req.params;
 
-  // Get personal details of customer 
-  const personalDetailsHost = process.env.PERSONAL_DETAILS_SERVICE_HOST
-    const personalDetailsPort = process.env.PERSONAL_DETAILS_SERVICE_PORT
-    let personalDetails = await fetch(`http://${personalDetailsHost}:${personalDetailsPort}/personalDetails/${id}`)
-    let pd = await personalDetails.json();
-  console.log(pd);
-    // Get Employment details of customer 
-  const employmentDetailsHost = process.env.EMPLOYMENT_DETAILS_SERVICE_HOST
-  const employmentDetailsPort = process.env.EMPLOYMENT_DETAILS_SERVICE_PORT
-  const message = await fetch(`http://${employmentDetailsHost}:${employmentDetailsPort}/employmentDetails/${id}`)
-    const employmentDetails = await message.json();
-    console.log(employmentDetails);
-    const response = {...pd, ...employmentDetails};
+  let personalDetails = await fetch(
+    `http://localhost:10000/personalDetails/${id}`
+  );
+  let pd = await personalDetails.json();
+
+  const message = await fetch(`http://localhost:10000/employmentDetails/${id}`);
+  const employmentDetails = await message.json();
+  console.log(employmentDetails);
+  const response = { ...pd, ...employmentDetails };
 
   res.send(response);
 });
